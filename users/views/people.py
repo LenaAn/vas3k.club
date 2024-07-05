6436bbe9d91cd02cb6fec1e0797e42695b3efc53
Elena Anyusheva <eanyusheva@confluent.io>
2024-07-05 18:37:46 -0400

    delete company from map_stat_group

--- users/views/people.py
+++ users/views/people.py
@@ -84,7 +84,6 @@ def people(request):
     users_total = users.count()
 
     map_stat_groups = {
-        "💼 Топ компаний": top(users, "company", skip={"-"})[:5],
         "🌍 Страны": top(users, "country")[:5],
         "🏰 Города": top(users, "city")[:5],
     }
